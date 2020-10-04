1c190955079f8319b54bd5520c79a2b521f7f3c8
Mark Beebe <mbeebe08@gmail.com>
2020-10-04 14:33:39 -0700

    Add ScoreViewModel.kt and display score functionality

--- app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
@@ -1,4 +1,14 @@
 package com.example.android.guesstheword.screens.score
 
-class ScoreViewModel {
+import android.util.Log
+import androidx.lifecycle.ViewModel
+
+class ScoreViewModel(finalScore: Int) : ViewModel() {
+    var score = finalScore
+    init {
+        Log.i("ScoreViewModel", "Final score is $finalScore")
+    }
+
+
+
 }
