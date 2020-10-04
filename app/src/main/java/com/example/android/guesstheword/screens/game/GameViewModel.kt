8cf90557298caa623257c7f2e83a7a1f7354e085
Mark Beebe <mbeebe08@gmail.com>
2020-10-04 15:55:22 -0700

    Change data in GameViewModel.kt to live data

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,11 +1,30 @@
 package com.example.android.guesstheword.screens.game
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import timber.log.Timber
 
 class GameViewModel : ViewModel() {
+    // The current word
+    private val _word = MutableLiveData<String>()
+    val word: LiveData<String>
+        get() = _word
+
+    // The current score
+    private val _score = MutableLiveData<Int>()
+    val score: LiveData<Int>
+        get() = _score
+
+    // The list of words - the front of the list is the next word to guess
+    private lateinit var wordList: MutableList<String>
+
+
     init {
+        _word.value = ""
+        _score.value = 0
+
         Log.i("GameViewModel", "GameViewModel Created")
         resetList()
         nextWord()
@@ -16,14 +35,6 @@ class GameViewModel : ViewModel() {
         Log.i("GameViewModel", "GameViewModel Destroyed")
     }
 
-    // The current word
-    var word = ""
-
-    // The current score
-    var score = 0
-
-    // The list of words - the front of the list is the next word to guess
-    private lateinit var wordList: MutableList<String>
 
     /**
      * Resets the list of words and randomizes the order
@@ -61,19 +72,19 @@ class GameViewModel : ViewModel() {
     private fun nextWord() {
         if (!wordList.isEmpty()) {
             //Select and remove a word from the list
-            word = wordList.removeAt(0)
+            _word.value = wordList.removeAt(0)
         }
     }
 
     /** Methods for buttons presses **/
 
     fun onSkip() {
-        score--
+        _score.value = (score.value)?.minus(1)
         nextWord()
     }
 
     fun onCorrect() {
-        score++
+        _score.value = (score.value)?.plus(1)
         nextWord()
     }
 
